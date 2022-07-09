b771a25304074e93ad952ffc88fbecf25eca874a
pinkipie11 <stani.simic22@gmail.com>
2022-07-09 09:50:21 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,6 +133,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 // Subscriber
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -142,6 +146,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
